5eb897cc6adff4a7f415b329d13a6da3ad99b8de
MegaISari <89361203+MegaISari@users.noreply.github.com>
2021-09-24 23:36:42 -0700

    The meal expenses

---
+++ meal.c
@@ -0,0 +1,68 @@
+#include <stdio.h> 
+
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+int breakfastAllowed(int timeDept, int timeArrival, int numDays);
+int lunchAllowed(int timeDept, int timeArrival, int numDays);
+int dinnerAllowed(int timeDept, int timeArrival, int numDays);
+int costAllowed(int timeDept, int timeArrival, int numDays);
+
+#endif
+
+int breakfastAllowed(int timeDept, int timeArrival, int numDays)
+{
+    int breakfast = 0; 
+
+    if (numDays > 2)
+        breakfast = breakfast + numDays -2;
+
+    if (timeDept < 7)
+        breakfast++;
+    
+    if (timeArrival > 8)
+        breakfast++;
+
+    return breakfast; 
+}
+
+int lunchAllowed(int timeDept, int timeArrival, int numDays)
+{
+    int lunch = 0; 
+
+    if (numDays > 2)
+        lunch = lunch + numDays -2;
+
+    if (timeDept < 7)
+        lunch++;
+    
+    if (timeArrival > 8)
+        lunch++;
+
+    return lunch; 
+}
+
+int dinnerAllowed(int timeDept, int timeArrival, int numDays)
+{
+    int dinner = 0; 
+
+    if (numDays > 2)
+        dinner = dinner + numDays -2;
+
+    if (timeDept < 7)
+        dinner++;
+    
+    if (timeArrival > 8)
+        dinner++;
+
+    return dinner; 
+}
+
+float costAlloted(int timeDept, int timeArrival, int numDays)
+{
+    int breakfastCost = breakfastAllowed(timeDept, timeArrival, numDays) * 9)
+    int lunchCost = lunchAllowed(timeDept, timeArrival, numDays) * 12)
+    int dinnerCost = dinnerAllowed(timeDept, timeArrival, numDays) * 16)
+    float sum = (float)breakfastCost + lunchCost + dinnerCost;
+    return sum; 
+}
