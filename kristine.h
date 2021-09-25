abbe0963b8ae4c4e7909d6cd3281d0883db64cc6
Kristine Trevino Kinoshita <kristinet@cpp.edu>
2021-09-25 10:23:29 -0700

    Created a header to contain my work for the project (seminar registration fees and hotel expenses)

---
+++ kristine.h
@@ -0,0 +1,5 @@
+#ifndef KRISTINE.H
+#define KRISTINE.H
+double seminarRegistrationFees(double totalExpenses);
+double hotelExpenses(int tripDays, double totalExpenses, double returnExpenses, double savedExpenses, double allowableExpenses);
+#endif
