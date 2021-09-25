e1e9158f659a83fd2de779d3cfbad6b96e4c572f
Kristine Trevino Kinoshita <kristinet@cpp.edu>
2021-09-25 16:48:47 -0700

    Changed double to float for all variables, added print statements for each variable, and changed the variables that hold the arrival and departure times into char arrays

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
+#include<string.h>
 #include "kristine.h"
  
  int main()
  {
-     double totalExpenses, savedExpenses, returnExpenses, allowedExpenses, arriveTime, departTime;
+     char arriveTime[5], departTime[5];
+     float totalExpenses, savedExpenses, returnExpenses, allowedExpenses;
      int tripDays;
+
+     printf("You have been on a trip for %d days from %s to %s.\n", tripDays, arriveTime, departTime);
+     printf("Total expenses: $%f\n", totalExpenses);
+     printf("Total allowable expenses: $%f\n", allowedExpenses);
+     printf("Amount reimbursement: $%f\n", returnExpenses);
+     printf("Amount saved: $%f\n", savedExpenses);
+
      return 0;
  }
