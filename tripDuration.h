63bce5fb9cc127dc0411a9805801a9e5bc5ef14d
CleoY <77142560+CleoY@users.noreply.github.com>
2021-09-25 13:30:53 -0700

    Added a tripDuration.c file for tracking the total number of days spent on a trip, the businessperson's arrival and departure times, and the cost of roundtrip airfare. Added a tripDuration.h header file, too.

---
+++ tripDuration.h
@@ -0,0 +1,9 @@
+#ifndef TRIPDURATION_H
+#define TRIPDURATION_H
+
+int tripDuration(int tripLength);
+char[] tripDeparture (char[]  departureTime);
+char[] tripArrival (char[]  arrivalTime);
+float totalAirface (float roundTripCost);
+
+#endif
