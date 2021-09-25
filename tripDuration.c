dd7aab770b8c83efbcdf1f5642716f0eaedb3fe3
CleoY <77142560+CleoY@users.noreply.github.com>
2021-09-25 15:07:44 -0700

    Updated tripDuration.c with function bodies.

--- tripDuration.c
+++ tripDuration.c
@@ -1,1 +1,33 @@
 #include "tripDuration.h"
+
+int tripLength;
+char[5] departureTime;
+char[5] arrivalTime;
+float roundTripCost;
+
+
+int tripDuration(){
+    printf("Please input the number of days the trip lasted, including the day of departure and the day of arrival: ");
+    scanf("%d", tripLength);
+    return tripLength;
+}
+
+int tripDeparture();
+    printf("Please input the time of departure in military hours with the format hour:minutes \n");
+    printf("Departure time: ");
+    scanf("%s", departureTime);
+    return 0;
+}
+
+int tripArrival(){
+    printf("Please input the time of arrival in military hours with the format hour:minutes \n");
+    printf("Arrival time: ");
+    scanf("%s", arrivalTime);
+    return 0;
+}
+                
+float totalAirfare(){
+    printf("Please enter the total cost of the roundtrip airfare: ");
+    scanf("%f", roundTripCost);
+    return roundTripCost;
+}
