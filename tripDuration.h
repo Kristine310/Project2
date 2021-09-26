299ec446924d5d1190a7c48c4386973e9ca6888c
CleoY <77142560+CleoY@users.noreply.github.com>
2021-09-26 01:47:12 -0700

    Updated tripDuration.h header file with user validation prototypes and variables.

--- tripDuration.h
+++ tripDuration.h
@@ -1,7 +1,7 @@
 #ifndef TRIPDURATION_H
 #define TRIPDURATION_H
 
-extern int tripLength;
+extern int tripLength, globalHours, globalMinutes;
 extern char departureTime[5];
 extern char arrivalTime[5];
 extern float roundTripCost;
@@ -10,5 +10,6 @@ int tripDuration();
 int tripDeparture();
 int tripArrival();
 float totalAirfare ();
+int timeValidation(char time[]);
 
 #endif
