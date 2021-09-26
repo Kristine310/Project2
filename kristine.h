a8eab27d1024d8465e95ce88e5f1ce9cfa9261c8
Kristine Trevino Kinoshita <kristinet@cpp.edu>
2021-09-26 13:37:41 -0700

    Added extern variables

--- kristine.h
+++ kristine.h
@@ -1,5 +1,9 @@
-#ifndef KRISTINE.H
-#define KRISTINE.H
-void semConRegistrationFees(float totalExpenses);
-void hotelExpenses(int tripDays, float totalExpenses, float returnExpenses, float savedExpenses, float allowableExpenses);
+#ifndef KRISTINE_H
+#define KRISTINE_H
+
+extern float hotelReturned, hotelSaved, hotelAllowed;
+
+float semConRegistrationFees(float totalExpenses);
+float hotelExpenses(int tripDays, float totalExpenses);
+
 #endif
