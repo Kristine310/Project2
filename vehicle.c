b22e1b5d99dfddc2404c123a5d10f5b477116c19
MegaISari <89361203+MegaISari@users.noreply.github.com>
2021-09-26 07:57:08 -0700

    vehicle changes

--- vehicle.c
+++ vehicle.c
@@ -7,21 +7,36 @@ The company allows up to $6 per day. Anything more than this must be paid by the
  a private vehicle was used. Calculate the vehicle expense as $0.27 per mile driven 
  */ 
 
+float vehicleExpense;
+float parkingExpense;
+
 //amount of any car rental 
-printf("Enter the total amount of any car rentals: \n");
-        scanf("%f", &rentalFare);
+int carRental(float totalExpenses) 
+{  int carRental; 
+    printf("Enter the total amount of any car rentals: \n");
+    scanf("%f", &carRental); 
+}
 
 // costs miles driven (covered)
- float costMilesCov(int totalMiles)
+ float costMilesCov(int tripDays, float totalMiles)
  {
+     printf("Enter the total costs of miles driven: ");
+     scanf("%f", &vehicleExpense);
      float vehicleExpense = 0;
      vehicleExpense = totalMiles * 0.27;
+      while(vehicleExpense<0){
+        printf("Error: Not a valid input! Airfare cannot be negative.\n");
+        printf("Please reenter the cost of airfare: ");
+        scanf("%f", &vehicleExpense);
+    }
      return vehicleExpense;
  }
 
 // costs parking (covered)
  float costParkingCov (int daysSpent)
  {
+     printf("Enter the total amount spent on Parking Fees: \n");
+        scanf("%f", &parkingExpense);
      float parkingExpense = 0;
      parkingExpense = daysSpent * 6.00;
      return parkingExpense;
@@ -30,7 +45,10 @@ printf("Enter the total amount of any car rentals: \n");
 // costs taxi (covered) 
  float costTaxisCov(int daysSpent)
  {
+     printf("Enter the total amount spent on Taxi Fees: \n");
+        scanf("%f", &taxiExpense);
      float taxiExpense = 0;
      taxiExpense = daysSpent * 10.00;
      return taxiExpense;
  }
+
