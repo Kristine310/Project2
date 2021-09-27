910466b42239442553fdf3c88d0c09a7efb123b1
MegaISari <89361203+MegaISari@users.noreply.github.com>
2021-09-26 18:22:31 -0700

    Update vehicle.h

--- vehicle1.h
+++ vehicle1.h
@@ -3,9 +3,9 @@
 
 extern float returnTaxiExpenses, returnParkExpenses, savedTaxiExpenses,savedParkExpenses, costParkAllowed, costTaxiAllowed; 
 
-float carRental(float totalExpenses)
-float costMiles(int tripDays, float totalExpenses)
-float costParking(int tripDays, float totalExpenses)
-float costTaxis(int tripDays, float totalExpenses)
+float carRental(float totalExpenses);
+float costMiles(int tripDays, float totalExpenses);
+float costParking(int tripDays, float totalExpenses);
+float costTaxis(int tripDays, float totalExpenses);
  
  #endif
